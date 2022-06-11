7c21466eac48e875cac6102a8cee7affac63b372
Alexandr <102781985+Eshenok@users.noreply.github.com>
2022-06-11 18:30:24 +0300

    done v 4.0

--- scripts/script.js
+++ scripts/script.js
@@ -1,24 +1,21 @@
 'use strict'
 
-const editPopupContainer = document.querySelector('.popup-edit-profile');
-const editPopupCloseButton = document.querySelector('.button_close_popup-edit-profile');
+const editPopupContainer = document.querySelector('.popup_type_profile');
+const editPopupCloseButton = document.querySelector('.button_close_profile');
 const editPopupProfileForm = document.querySelector('.popup__form_type_edit-profile');
 const editPopupOpenButton = document.querySelector('.button_icon_edit');
 const editPopupProfileName = document.querySelector('.profile__name');
 const editPopupProfileOccupation = document.querySelector('.profile__occupation');
 const editPopupInputName = document.querySelector('.popup__input_type_name');
 const editPopupInputOccupation = document.querySelector('.popup__input_type_occupation'); //popup редактирования профиля
-
-
-const addPopupContainer = document.querySelector('.popup-add-content');
-const addPopupButtonClose = document.querySelector('.button_close_popup-add-content');
+const addPopupContainer = document.querySelector('.popup_type_card');
+const addPopupButtonClose = document.querySelector('.button_close_card');
 const addPopupOpenButton = document.querySelector('.button_icon_add');
 const addPopupImageCaption = document.querySelector('.popup__input_type_image-caption');
 const addPopupImageSrc = document.querySelector('.popup__input_type_image-src');
 const addPopupForm = document.querySelector('.popup__form_type_add-content');// popup Добавления контента
-const popupPhotoGrid = document.querySelector('.popup-photo-grid');
-const popupPhotoGridCloseButton = document.querySelector('.button_close_popup-photo-grid');
-
+const imagePopupContainer = document.querySelector('.popup_type_image');//popup картинки
+const imagePopupCloseButton = document.querySelector('.button_close_image');
 const photoGridSection = document.querySelector('.photo-grid');
 const photoGridTemplate = document.querySelector('#photo-grid__template');// template
 const initialCards = [
@@ -56,68 +53,64 @@ function openPopup (popup) {
   popup.classList.add('popup_opened'); /*вывод попапа*/
 }
 
-function editProfileInfo () {
-  openPopup(editPopupContainer);
+function openPopupProfile () {
   editPopupInputName.value = editPopupProfileName.textContent; /*присваивание имени*/
   editPopupInputOccupation.value = editPopupProfileOccupation.textContent; /*присваивание брифа*/
+  openPopup(editPopupContainer);
 }
 
-function editPopupFormSubmitHandler (evt) {
+function changeProfileValue (evt) {
   evt.preventDefault(); 
   editPopupProfileName.textContent = editPopupInputName.value; /*Замена имени*/
   editPopupProfileOccupation.textContent = editPopupInputOccupation.value; /*Замена брифа*/
   closePopup(editPopupContainer);
 }
 
-function addPopupFormSubmitHandler (evt) {
+function makeCard (cardImageSource, cardCaptionSource) {
+  const card = photoGridTemplate.content.cloneNode(true); //clone template
+  const cardImage = card.querySelector('.photo-grid__image');
+  const likeButton = card.querySelector('.button_icon_like');
+  const deleteButton = card.querySelector('.button_icon_delete');
+  card.querySelector('.photo-grid__caption').textContent = cardCaptionSource; //caption
+  cardImage.src = cardImageSource; // src
+  cardImage.alt = cardCaptionSource; // alt
+  cardImage.addEventListener('click', function () {
+    const imagePopup = imagePopupContainer.querySelector('.popup__image');
+    const captionPopup = imagePopupContainer.querySelector('.popup__caption');
+    imagePopup.src = cardImage.src;
+    captionPopup.textContent = cardImage.alt;
+    imagePopupContainer.classList.add('popup_opened');
+  });
+  likeButton.addEventListener('click', function () { // like
+    likeButton.classList.toggle('button_icon_like-active');
+  });
+  deleteButton.addEventListener('click', function () {
+    deleteButton.parentNode.remove();
+  });
+  return card;
+}
+
+function addNewCard (evt) {
   evt.preventDefault();
-  const clonePhotoGrid = photoGridTemplate.content.cloneNode(true); //clone template
-  clonePhotoGrid.querySelector('.photo-grid__caption').textContent = addPopupImageCaption.value; //caption
-  clonePhotoGrid.querySelector('.photo-grid__image').setAttribute('src', addPopupImageSrc.value); // src
-  clonePhotoGrid.querySelector('.photo-grid__image').setAttribute('alt', addPopupImageCaption.value); // alt
-  clonePhotoGrid.querySelector('.photo-grid__image').addEventListener('click', function (evt) {
-    popupPhotoGrid.querySelector('.popup-photo-grid__image').setAttribute('src', evt.target.getAttribute('src'));
-    popupPhotoGrid.querySelector('.popup-photo-grid__caption').textContent = evt.target.getAttribute('alt');
-    popupPhotoGrid.classList.add('popup_opened');
-  });
-  clonePhotoGrid.querySelector('.button_icon_like').addEventListener('click', function (evt) { // like
-    evt.target.classList.toggle('button_icon_like-active');
-  });
-  clonePhotoGrid.querySelector('.button_icon_delete').addEventListener('click', function (evt) {
-    evt.target.parentNode.remove();
-  });
-  photoGridSection.prepend(clonePhotoGrid); // add content in html
+  const card = makeCard(addPopupImageSrc.value, addPopupImageCaption.value)
+  photoGridSection.prepend(card); // add content in html
   closePopup(addPopupContainer);
+  addPopupImageCaption.value = '';
+  addPopupImageSrc.value = '';
 }
 
-function preloadImage () {
+function preloadImages () {
   initialCards.forEach(function (elem) { //for(i = 0; i < initialCards.lenghth; i++)
-  const clonePhotoGrid = photoGridTemplate.content.cloneNode(true); //clone template
-  clonePhotoGrid.querySelector('.photo-grid__caption').textContent = elem.name; // caption
-  clonePhotoGrid.querySelector('.photo-grid__image').setAttribute('src', elem.link); //src
-  clonePhotoGrid.querySelector('.photo-grid__image').setAttribute('alt', elem.name); //alt
-  clonePhotoGrid.querySelector('.photo-grid__image').addEventListener('click', function (evt) {
-    popupPhotoGrid.querySelector('.popup-photo-grid__image').setAttribute('src', evt.target.getAttribute('src'));
-    popupPhotoGrid.querySelector('.popup-photo-grid__caption').textContent = evt.target.getAttribute('alt');
-    popupPhotoGrid.classList.add('popup_opened');
-  });
-  clonePhotoGrid.querySelector('.button_icon_like').addEventListener('click', function (evt) { // like
-    evt.target.classList.toggle('button_icon_like-active');
-  });
-  clonePhotoGrid.querySelector('.button_icon_delete').addEventListener('click', function (evt) {
-    evt.target.parentNode.remove();
-  });
-  photoGridSection.prepend(clonePhotoGrid); //add content in html
+  const card = makeCard(elem.link, elem.name);
+  photoGridSection.prepend(card); //add content in html
   });
 }
 
-preloadImage();
-editPopupOpenButton.addEventListener('click', editProfileInfo);
-editPopupCloseButton.addEventListener('click', function () {closePopup(editPopupContainer)});
-editPopupProfileForm.addEventListener('submit', editPopupFormSubmitHandler);//popup редактирования профиля
-
-addPopupOpenButton.addEventListener('click', function () {openPopup(addPopupContainer)});
-addPopupButtonClose.addEventListener('click', function () {closePopup(addPopupContainer)});
-addPopupForm.addEventListener('submit', addPopupFormSubmitHandler);//popup добавления контента
-
-popupPhotoGridCloseButton.addEventListener('click', function () {closePopup(popupPhotoGrid)})
+preloadImages();
+editPopupOpenButton.addEventListener('click', openPopupProfile);
+editPopupCloseButton.addEventListener('click', () => {closePopup(editPopupContainer)});
+editPopupProfileForm.addEventListener('submit', changeProfileValue);//popup редактирования профиля
+addPopupOpenButton.addEventListener('click', () => {openPopup(addPopupContainer)});
+addPopupButtonClose.addEventListener('click', () => {closePopup(addPopupContainer)});
+addPopupForm.addEventListener('submit', addNewCard);//popup добавления контента
+imagePopupCloseButton.addEventListener('click', () => {closePopup(imagePopupContainer)})
